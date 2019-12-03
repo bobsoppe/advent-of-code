cdd804f937256ab872963860f8664bee357bc3f5
Bob Soppe <bobsoppe@users.noreply.github.com>
2019-12-03 15:33:01 +0100

    Add solution for day 03 for Advent of Code 2019

---
+++ advent-of-code-2019/src/advent_of_code_2019/day_03.clj
@@ -0,0 +1,56 @@
+(ns advent-of-code-2019.day-03
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clojure.edn :as edn]))
+
+(def input
+  (-> "advent_of_code_2019/day_03.txt"
+      io/resource io/file slurp
+      (str/split-lines)
+      (->>
+        (map #(str/split % #",")))))
+
+(defn calculate-steps [[x y] direction distance]
+  (map (fn [step]
+         (case direction
+           "U" [x (+ y step)]
+           "R" [(+ x step) y]
+           "D" [x (- y step)]
+           "L" [(- x step) y]))
+       (range 1 (+ 1 distance))))
+
+(defn calculate-coordinates [line]
+  (reduce (fn [coordinates next-step]
+            (vec
+              (concat coordinates
+                      (calculate-steps
+                        (last coordinates)
+                        (first next-step)
+                        (last next-step)))))
+          [[0 0]]
+          (map (fn [[direction & distance]] [(str direction) (read-string (apply str distance))]) line)))
+
+(defn calculate-intersections [line-1 line-2]
+  (let [line-2 (set line-2)]
+    (->> (filter (fn [point] (contains? line-2 point)) line-1)
+        (drop 1))))
+
+(defn calculate-manhattan-distance [[x y]]
+  (+ (Math/abs x) (Math/abs y)))
+
+(defn calculate-steps-for-intersection [line-1 line-2 intersection]
+  (let [line-1-steps (.indexOf line-1 intersection)
+        line-2-steps (.indexOf line-2 intersection)]
+    (+ line-1-steps line-2-steps)))
+
+(println "Solution part 1\n"
+         (let [[line-1 line-2] (map calculate-coordinates input)]
+           (->> (calculate-intersections line-1 line-2)
+                (map calculate-manhattan-distance)
+                (reduce min))))
+
+(println "Solution part 2\n"
+         (let [[line-1 line-2] (map calculate-coordinates input)]
+           (->> (calculate-intersections line-1 line-2)
+                (map (fn [intersection] (calculate-steps-for-intersection line-1 line-2 intersection)))
+                (reduce min))))
